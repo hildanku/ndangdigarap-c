fbd2b17e1147a0bbb5bb58efb256b45be7208be4
hildanku <hildankutomo@gmail.com>
2025-01-24 13:18:54 +0700

    improve: added db status checking if down

--- auth-service/handlers/healthcheck.go
+++ auth-service/handlers/healthcheck.go
@@ -3,9 +3,30 @@ package handlers
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/hildanku/ndangdigarap/utils"
+	"gorm.io/gorm"
 )
 
 func Hello(c *fiber.Ctx) error {
 	null := 0
 	return utils.AppResponse(c, fiber.StatusOK, "up!", null)
+}
+
+func HealthCheck(db *gorm.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		sqlDB, err := db.DB()
+		if err != nil || sqlDB.Ping() != nil {
+			return utils.AppResponse(
+				c,
+				fiber.StatusInternalServerError,
+				"Database is down",
+				nil,
+			)
+		}
+		return utils.AppResponse(
+			c,
+			fiber.StatusOK,
+			"Database is up",
+			nil,
+		)
+	}
 }

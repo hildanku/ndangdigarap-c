54f4a53864af12ed020b9e744f9588a496ed4665
hildanku <hildankutomo@gmail.com>
2025-02-27 01:10:21 +0700

    improve: add logger hono

--- task-service/src/main.ts
+++ task-service/src/main.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono'
 import taskRoutes from './module/tasks/task.route'
+import { logger } from 'hono/logger'
+
 
 const app = new Hono()
-
+app.use(logger())
 app.route('/api', taskRoutes)
 
 export default {
